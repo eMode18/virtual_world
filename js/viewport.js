8a273a6dabd32b15c238c7e251c8129f21d0cb8d
eMode18 <ewagandi@gmail.com>
2025-01-03 12:24:32 +0300

    added drag motion

--- js/viewport.js
+++ js/viewport.js
@@ -5,11 +5,62 @@ class Viewport {
 
     this.zoom = 1;
 
+    this.center = new Point(canvas.width / 2, canvas.height / 2);
+
+    this.offset = new Point(0, 0);
+
+    this.drag = {
+      start: new Point(0, 0),
+      end: new Point(0, 0),
+      offset: new Point(0, 0),
+      active: false,
+    };
+
     this.#addEventListeners();
+  }
+
+  getMouse(e) {
+    return new Point(e.offsetX * this.zoom, e.offsetY * this.zoom);
+  }
+
+  getOffset() {
+    return add(this.offset, this.drag.offset);
   }
 
   #addEventListeners() {
     this.canvas.addEventListener("wheel", this.#handleMouseWheel.bind(this));
+
+    this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+
+    this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
+
+    this.canvas.addEventListener("mouseup", this.#handleMouseUp.bind(this));
+  }
+
+  #handleMouseDown(event) {
+    if (EventTarget.button == 1) {
+      this.drag.start = this.getMouse(evt);
+      this.drag.active = true;
+    }
+  }
+
+  #handleMouseMove(event) {
+    if (this.drag.active) {
+      this.drag.end = this.getMouse(event);
+      this.drag.offset = this.drag.end.subtract(this.drag.start);
+    }
+  }
+
+  #handleMouseUp(event) {
+    if (this.drag.active) {
+      this.offset = this.offset.add(this.drag.offset);
+      this.drag = {
+        start: new Point(0, 0),
+        end: new Point(0, 0),
+        offset: new Point(0, 0),
+        active: false,
+      };
+    }
   }
 
   #handleMouseWheel(event) {
